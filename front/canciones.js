2dd09cda8a52092fa257f8c009ce8af9111c5944
underdough <k3vinch3nl1@gmail.com>
2026-04-24 02:01:36 -0500

    feat: add song list configuration and fix video source filename in index.html

--- front/canciones.js
+++ front/canciones.js
@@ -43,6 +43,27 @@ window.CANCIONES = [
 
   { titulo: 'A VOS -- Milo J', videoId: 'IyKkj3fZX1w' },
 
+  { titulo: 'DESEO PERDER [INTERLUDIO] -- Milo J', videoId: '-zxlKAR5e2w' },
+
+  { titulo: 'BESAME REMIX -- BHAVI, SEVEN KAYNE, MILO J, TIAGO PZK, KHEA, NEO PISTEA', videoId: '9hxzmqpnGb0' },
+
+  // Andrés Cepeda
+  { titulo: 'Tengo ganas -- Andrés Cepeda', videoId: 'S0Ps_5ABIbE' },
+
+  // Manuel Medrano
+  { titulo: 'La distancia  -- Manuel Medrano', videoId: 'dH7_By3Y2i0' },
+
+  { titulo: 'La mujer que bota fuego -- Manuel Medrano', videoId: 'MkloKqdbJWo' },
+
+  // Salsa
+  { titulo: 'Anhelo  -- Adolescents Orquesta', videoId: '2Dvy5lRuaOI' },
+
+  // WOS
+  { titulo: 'Alma dinamita -- WOS', videoId: 'Hmc2UKFNGzM' },
+
+  // DAniel, Me estás matando
+  { titulo: 'Es verdad -- Daniel, Me estás matando', videoId: 'xV9mqKWBqaw' },
+
 
   // Mas canciones - busca en YouTube y agrega el ID
   // { titulo: 'Nombre de la cancion', videoId: 'ID_DEL_VIDEO' },
